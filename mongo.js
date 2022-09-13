302b21231ed36117c43da90d018ea16efa1384ab
skarussel <steven.kocadag@st.ovgu.de>
2022-09-13 14:30:42 +0300

    My first commit

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 5) {
+  console.log('Please provide the password as an argument: node mongo.js <password> <name> <number>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://Junckie:${password}@cluster0.gd7lm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const name = process.argv[3]
+const number = process.argv[4]
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected')
+
+    const person = new Person({
+      name: name,
+      number: number
+    })
+
+    return person.save()
+  })
+  .then(() => {
+    console.log(`added ${name} ${number} to phonebook`)
+    return mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))
